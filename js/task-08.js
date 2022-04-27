712f1c8bf6ff087d80535fe5dc83f8b90aa651d2
Serhii Bulychov <woyager69@gmail.com>
2022-04-27 04:36:21 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,7 @@
 
+const form = document.querySelector(".login-form");
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefolt();
+    const formData = new FormData(event.currentTarget);
+}
