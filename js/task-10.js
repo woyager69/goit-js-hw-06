9292fc4627f235b26893319915757370b7a7a42a
Serhii Bulychov <woyager69@gmail.com>
2022-04-27 14:39:12 +0300

    task8

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// let boxsize = 30;
+// for (let i = 0; i <= quantity - 1; i++){
+//         const createDiv = document.createElement('div');
+//     createDiv.style.width =`${boxsize}px`;
+//         createDiv.style.height =`${boxsize}px`;
+//         createDiv.style.backgroundColor = getRandomHexColor();
+//     array.push(createDiv);
+//     boxsize += 10;
+//   };
+let boxSize = 30;
+for (let i = 0; i < amount; i += 1) {
+		markup.push(
+			`<div style="width: ${boxSize}px; height: ${boxSize}px; background-color: ${getRandomHexColor()}"></div>`
+		);
+		boxSize += 10;
+	}
