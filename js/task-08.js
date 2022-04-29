e8139434393555be121f02ed34d8a38dc1db87a6
Serhii Bulychov <woyager69@gmail.com>
2022-04-29 10:37:04 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,7 +1,14 @@
-
 const form = document.querySelector(".login-form");
 form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-}
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+    console.log({ email: email.value, password: password.value });
+    event.currentTarget.reset();
+}   
--- js/task-10.js
+++ js/task-10.js
@@ -3,39 +3,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-// let boxsize = 30;
-// for (let i = 0; i <= quantity - 1; i++){
-//         const createDiv = document.createElement('div');
-//     createDiv.style.width =`${boxsize}px`;
-//         createDiv.style.height =`${boxsize}px`;
-//         createDiv.style.backgroundColor = getRandomHexColor();
-//     array.push(createDiv);
-//     boxsize += 10;
-//   };
-const inputEl = document.querySelector('input');
-const addBtn = document.querySelector('button[data-create]');
-const delBtn = document.querySelector('button[data-destroy]');
-const boxesEl = document.querySelector('[boxes]');
-let amount = inputEl.textContent;
-// let amount = inputEl.currentTarget.value;
-addBtn.addEventListener("click", () => createBoxes(amount));
-function createBoxes() {
-	let boxSize = 30;
-for (let i = 0; i < amount; i += 1) {
-		markup.push(
-			`<div style="width: ${boxSize}px;
-			             height: ${boxSize}px; 
-						 background-color: ${getRandomHexColor()}"></div>`
-		);
-		boxSize += 10;
-	}
-}
-
-
-// let boxSize = 30;
-// for (let i = 0; i < amount; i += 1) {
-// 		markup.push(
-// 			`<div style="width: ${boxSize}px; height: ${boxSize}px; background-color: ${getRandomHexColor()}"></div>`
-// 		);
-// 		boxSize += 10;
-// 	}
